de05d15c291e1819f74a6e6625c1e120ebd6e07d
Shaan Sundar <40330352+shaansundar@users.noreply.github.com>
2025-07-01 16:10:30 +0530

    feat: add shaan-sundar team submission (#3)
    * submission: Shaan Sundar

---
+++ public-multisig-shaan/README.md
@@ -0,0 +1,20 @@
+# Public Treasury Multisig
+This is a prototype on how a Multisig contract can cross interact with a Decentralized Autonomous Organization.
+
+Loom Link: [https://www.loom.com/share/04205f260ee64246a168d5ade2966027?sid=2b867757-976a-42f5-a9d0-d4657bf3d4ea](https://www.loom.com/share/04205f260ee64246a168d5ade2966027?sid=2b867757-976a-42f5-a9d0-d4657bf3d4ea)
+
+Prototype Access Link: []()
+
+## Steps to run
+Assuming the environment is setup properly with the required node configurations, one can run:
+```
+cd contracts && bunx hardhat bunx hardhat run deploy/index.ts
+```
+and
+```
+cd ../frontend && bun run dev
+```
+
+To get the frontend spun up on `http://localhost:3000`
+
+
---
+++ public-multisig-shaan/frontend/src/hooks/getTransactions.ts
@@ -0,0 +1,54 @@
+import { useReadContract } from "wagmi";
+import { readContract } from "wagmi/actions";
+import { multiSigWallet } from "@/lib/multisig";
+import { useQuery } from "@tanstack/react-query";
+import { config } from "@/config/rainbowkit";
+
+export interface Transaction {
+    to: string;
+    value: bigint;
+    title: string;
+    description: string;
+    type: string;
+    executed: boolean;
+    approvalCount: number;
+}
+export const useGetTransactions = () => {
+
+    const { data: transactionCount } = useReadContract({
+        address: multiSigWallet.address as `0x${string}`,
+        abi: multiSigWallet.abi,
+        functionName: "getTransactionCount",
+        args: [],
+    });
+
+    return useQuery({
+        queryKey: ["transactions"],
+        queryFn: async () => {
+            const transactions = [];
+            if (!transactionCount) return [];
+            for (let i = 0; i < Number(transactionCount); i++) {
+                // @ts-expect-error: This is a workaround to fix the type error
+                const transaction : [string, bigint, string, string, string, boolean, number] = await readContract(config,{
+                    address: multiSigWallet.address as `0x${string}`,
+                    abi: multiSigWallet.abi,
+                    functionName: "transactions",
+                    args: [i],
+                });
+                transactions.push({
+                    to: transaction[0] as `0x${string}`,
+                    value: transaction[1] as bigint,
+                    title: transaction[2] as string,
+                    description: transaction[3] as string,
+                    type: transaction[4] as string,
+                    executed: transaction[5] as boolean,
+                    approvalCount: transaction[6] as number,
+                });
+                // convert array to TransactionType
+            }
+            return transactions as Transaction[];
+        },
+        enabled: !!transactionCount,
+        refetchInterval: 1000,
+    });
+}
